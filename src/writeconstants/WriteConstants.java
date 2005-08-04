c758f74660ccc92fc6ad6ca5f19184947a69accd
Y. Zwols <yzwols@gmail.com>
2005-08-04 15:27:20 +0000

    *** empty log message ***

---
+++ src/writeconstants/WriteConstants.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.text.*;
+import java.io.*;
+
+  public class WriteConstants {
+
+	public static void main ( String args [ ] ) {
+
+		if (args.length != 1) {
+			System.out.println("Usage: WriteConstants <version>");
+			return;
+		}
+
+		String date = GetDate();
+
+		System.out.println("/* Generated code -- do not edit */");
+		System.out.println("public class Constants {");
+		System.out.println("    public static final String COMPILETIME = \"" + date + "\";");
+		System.out.println("    public static final String VERSION = \"" + args[0] + "\";");
+		System.out.println("}");
+	}
+
+	private static String GetDate() {
+		Date date = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yy");
+		return sdf.format(date);
+	}
+}
+
