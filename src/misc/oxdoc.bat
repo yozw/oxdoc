8054de1bdf2476680bf395bb38b4a47ab54c2b7d
Y. Zwols <yzwols@gmail.com>
2010-01-30 18:42:50 +0000

    *** empty log message ***

--- src/misc/oxdoc.bat
+++ src/misc/oxdoc.bat
@@ -1,3 +1,3 @@
 @echo off
 set oxdoc=c:\program files\oxdoc
-java -classpath "%oxdoc%\bin\oxdoc.jar;%oxdoc%\bin\jregex.jar" OxDocCmd %1 %2 %3 %4 %5 %6 %7 %8 %9
+java -jar "%oxdoc%\bin\oxdoc.jar" %1 %2 %3 %4 %5 %6 %7 %8 %9
